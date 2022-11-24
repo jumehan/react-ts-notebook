82696572e7c4a1717b9ebd31294fd9f0f08192e0
Julie Han <juliemenghan@gmail.com>
2022-11-24 14:04:04 -0500

    add localstorage for storing notes
    add tags
    add typing for notes
    add state for notes and tags

--- notes/src/App.tsx
+++ notes/src/App.tsx
@@ -1,15 +1,44 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useState } from "react"
 import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
+import useLocalStorage from "./useLocalStorage"
 import NewNote from "./NewNote"
 
+type Note = {
+  id: string;
+} & NoteData
+
+type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+}
+
+type RawNote = {
+  id: string;
+}
+
+type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+}
+
+type Tag = {
+  id: string;
+  label: string;
+}
+
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote/>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
@@ -21,3 +50,10 @@ function App() {
 }
 
 export default App
+export type {
+  Note,
+  NoteData,
+  RawNote,
+  RawNoteData,
+  Tag,
+}
--- notes/src/NoteForm.tsx
+++ notes/src/NoteForm.tsx
@@ -1,32 +1,63 @@
-import { useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, FormControl, FormGroup, FormLabel, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import CreatableReactSelect from "react-select"
+import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "./App";
 
-function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void
+}
+
+function NoteForm({ onSubmit }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null)
   const mardownRef = useRef<HTMLTextAreaElement>(null)
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: mardownRef.current!.value,
+      tags: [],
+    })
+  }
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <FormGroup controlId="title">
               <FormLabel>Title</FormLabel>
-              <FormControl required />
+              <FormControl ref={titleRef} required />
             </FormGroup>
           </Col>
           <Col>
             <FormGroup controlId="tags">
               <FormLabel>Tags</FormLabel>
-              <CreatableReactSelect isMulti />
+              <CreatableReactSelect
+                value={selectedTags.map(tag => {
+                  return {
+                    label: tag.label,
+                    value: tag.id,
+                  }
+                })}
+                onChange={tags => {
+                  setSelectedTags(tags.map(tag => {
+                    return {
+                      label: tag.label,
+                      id: tag.value,
+                    }
+                  }))
+                }}
+                isMulti
+              />
             </FormGroup>
           </Col>
         </Row>
         <FormGroup controlId="markdown">
           <FormLabel>Body</FormLabel>
-          <FormControl required as="textarea" rows={15} />
+          <FormControl ref={mardownRef} required as="textarea" rows={15} />
         </FormGroup>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
