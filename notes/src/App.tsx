a66bad2b8a6c38511550db7be4640e722b5c89ed
Julie Han <juliemenghan@gmail.com>
2022-12-04 20:33:59 -0500

    add update note fn()

--- notes/src/App.tsx
+++ notes/src/App.tsx
@@ -6,6 +6,9 @@ import { v4 as uuidv4 } from "uuid";
 import useLocalStorage from "./useLocalStorage";
 import NewNote from "./NewNote";
 import NoteList from "./NoteList";
+import NoteLayout from "./NoteLayout";
+import Note from "./Note";
+import EditNote from "./EditNote";
 
 type Note = {
   id: string;
@@ -54,6 +57,18 @@ function App() {
     });
   }
 
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return { ...note, ...data, tagIds: tags.map((tag) => tag.id) }
+        } else {
+          return note;
+        }
+      });
+    });
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -75,9 +90,18 @@ function App() {
             />
           }
         />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note />} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={onUpdateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
